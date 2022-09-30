75d7abed40f3e0c0f78ff883182a3cc14ff5d02b
Kel Cecil <kelcecil@praisechaos.com>
2022-09-30 09:55:37 -0400

    Add argon2 for storing passwords

--- lib/video_game_organizer/user.ex
+++ lib/video_game_organizer/user.ex
@@ -18,5 +18,21 @@ defmodule VideoGameOrganizer.User do
     |> validate_required([:email])
     |> validate_format(:email, ~r/@/)
     |> validate_length(:password, min: 12)
+    |> maybe_put_password_hash()
+  end
+
+  defp maybe_put_password_hash(%Ecto.Changeset{valid?: true} = changeset) do
+    case get_change(changeset, :password) do
+      nil ->
+        changeset
+
+      password ->
+        hashed_password = Argon2.hash_pwd_salt(password)
+        put_change(changeset, :password_hash, hashed_password)
+    end
+  end
+
+  defp maybe_put_password_hash(changeset) do
+    changeset
   end
 end
